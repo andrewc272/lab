5506977467f1c00b73de17118621628ca86050a1
Andrew Carlson <andrewcarlson272@gmail.com>
2022-11-18 14:43:14 -0600

    Taking out type hinting until later

--- account.py
+++ account.py
@@ -1,25 +1,25 @@
 
 class Account:
-    def __init__(self, name: str) -> None:
+    def __init__(self, name):
         self.__account_name = name
         self.__account_balance = 0
 
-    def deposit(self, amount: float) -> bool:
+    def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount: float) -> bool:
+    def withdraw(self, amount):
         if self.__account_balance - amount < 0 or amount <= 0:
             return True
         else:
             self.__account_balance += -1*amount
             return True
 
-    def get_name(self) -> str:
+    def get_name(self):
         return self.__account_name
 
-    def get_balance(self) -> float:
+    def get_balance(self):
         return self.__account_balance
